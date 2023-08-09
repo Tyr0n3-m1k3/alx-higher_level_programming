80eecd176c2201550d06ffe6a0f75c3cbd224072
Tyr0n3-m1k3 <truthuthi@gmail.com>
2023-08-09 03:20:44 -0700

    hello

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,3 @@
+#!/usr/bin/bash/python3
+def magic_calculation(a, b):
+    return (98 + a ** b)
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/bin/bash/python3
+str1 = "Holberton"
+str2 = "School!"
+#The two strings are added using print
+print(f"Welcome to {str1} {str2}")
